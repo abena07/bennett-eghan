d79588432926cb1a021611e42d33d3f3d345a33b
abena07 <abenabennett@gmail.com>
2025-03-15 16:20:36 +0000

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+function App() {
+
+  return (
+    <>
+     
+      
+     <div className="font-sans text-2xl font-bold">This text uses Source Sans 3 great!</div>
+
+
+   
+      
+
+
+
+    </>
+  )
+}
+
+export default App
