d0344931855dc37dcd3176bab6bfd1050b5cc32c
Bennett-Eghan <62352130+abena07@users.noreply.github.com>
2025-12-08 17:04:04 +0000

    fix deploy

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import { ExternalLink, Github } from "lucide-react";
-import { title } from "process";
 import { Helmet } from "react-helmet-async";
 
 function Projects() {
